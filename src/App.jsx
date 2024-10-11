9e1ffd9b1bca47ec684ae2af643fcfff4e6ccf04
Z1rael88 <oleksiibahmet@gmai.com>
2024-10-11 08:27:04 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Zirka Marketplace</div>
+    </>
+  );
+}
+
+export default App;
